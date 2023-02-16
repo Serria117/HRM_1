f5182dff03f8b3cf628fc26c713d7c6219742ecd
ThanhHa <june25th87@gmail.com>
2023-02-16 15:44:42 +0700

    update BaseEntity

--- src/main/java/com/hrm/entities/BaseEntity.java
+++ src/main/java/com/hrm/entities/BaseEntity.java
@@ -3,30 +3,37 @@ package com.hrm.entities;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.Authentication;
 
+import javax.annotation.PostConstruct;
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@MappedSuperclass @Getter @Setter @Accessors(chain = true)
+@MappedSuperclass
+@Getter @Setter @Accessors(chain = true)
 public abstract class BaseEntity
 {
     protected Boolean isActivated = true;
     protected Boolean isDeleted = false;
-    protected LocalDateTime createdTime = LocalDateTime.now();
+    @CreationTimestamp
+    @Column(updatable = false)
+    protected LocalDateTime createdTime;
+    @UpdateTimestamp
     protected LocalDateTime lastModifiedTime;
     protected String createdByUser;
     protected String lastModifiedByUser;
 
+    @PostConstruct
     public void setCreation(Authentication authentication)
     {
         this.createdByUser = authentication.getName();
-        this.createdTime = LocalDateTime.now();
     }
 
     public void setModification(Authentication authentication)
     {
         this.lastModifiedByUser = authentication.getName();
-        this.lastModifiedTime = LocalDateTime.now();
     }
 }
