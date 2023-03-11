fe5d8a40020254afef3c70c384e67ae483fe28fe
ThanhHa <june25th87@gmail.com>
2023-03-11 22:20:36 +0700

    update CommonUtils: get userID from authentication

--- src/main/java/com/hrm/utils/CommonUtils.java
+++ src/main/java/com/hrm/utils/CommonUtils.java
@@ -1,14 +1,28 @@
 package com.hrm.utils;
 
+import com.hrm.security.AppUserDetails;
+import lombok.experimental.UtilityClass;
 import org.springframework.security.core.Authentication;
 
+import java.util.UUID;
+
+@UtilityClass
 public class CommonUtils
 {
-    public static String getUsername(Authentication auth)
+    public String getUsername(Authentication auth)
     {
         if ( auth.isAuthenticated() ) {
             return auth.getName();
         }
         return "<No user logged in>";
+    }
+
+    public UUID getUserId(Authentication auth)
+    {
+        if ( auth.isAuthenticated() ) {
+            var user = (AppUserDetails) auth.getPrincipal();
+            return user.getUserId();
+        }
+        return null;
     }
 }
