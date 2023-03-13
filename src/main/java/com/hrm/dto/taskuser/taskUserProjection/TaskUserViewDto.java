8468f138573c4b1d60453af3ce38baf0d3dd2787
ThanhHa <june25th87@gmail.com>
2023-03-13 18:44:51 +0700

    fix query

--- src/main/java/com/hrm/dto/taskuser/taskUserProjection/TaskUserViewDto.java
+++ src/main/java/com/hrm/dto/taskuser/taskUserProjection/TaskUserViewDto.java
@@ -1,11 +1,9 @@
 package com.hrm.dto.taskuser.taskUserProjection;
 
-import java.util.UUID;
-
 public interface TaskUserViewDto {
-    Long taskId();
-    UUID assignById();
-    String taskName();
-    String assignByName();
-    String taskDescription();
+    Long getTaskId();
+    String getAssignById();
+    String getTaskName();
+    String getAssignByName();
+    String getTaskDescription();
 }
